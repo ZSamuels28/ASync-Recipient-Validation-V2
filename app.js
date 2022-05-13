3a1f36f33225da2e595a4f499604e5d96e58be71
ZSamuels28 <ZSamuels28@gmail.com>
2022-05-13 14:18:53 -0700

    Added comments

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const validateRecipients = require("./scripts/validateRecipients");
-const myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2); //Pulls in command line arguments
 
 if (
   myArgs[0] == "-i" &&
@@ -7,21 +7,24 @@ if (
   myArgs[2] == "-o" &&
   myArgs[3] != null
 ) {
-  let count = 0;
+  //If the command line arguments are formatted properly
+  let count = 0; //Line count
   require("fs")
     .createReadStream(myArgs[1])
     .on("data", function (chunk) {
       for (let i = 0; i < chunk.length; ++i) if (chunk[i] == 10) count++;
-    })
+    }) //Reads the infile and increases the count for each line
     .on("close", function () {
+      //At the end of the file, after all lines have been counted, run the recipient validation function
       validateRecipients.validateRecipients(count, myArgs);
     });
 } else if (myArgs[0] == "-h") {
+  //If the first command line argument is -h
   console.log(
     "\x1b[31mThe following is how to use this app.\n\nPlease provide the following arguments:\n\nnode ./app.js -i infile.csv -o outfile.csv"
   );
-  exit(0);
 } else {
+  //If the command line arguments are improperly formatted
   console.log(
     "\x1b[31mPlease provide the following arguments:\n\nnode ./app.js -i infile.csv -o outfile.csv"
   );
--- scripts/validateRecipients.js
+++ scripts/validateRecipients.js
@@ -4,54 +4,63 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 const path = require("path");
 const { JSDOM } = require("jsdom");
-dotenv.config({ path: path.resolve(__dirname, "../config/variables.env") });
-const SPARKPOST_API_KEY = process.env.SPARKPOST_API_KEY;
-const SPARKPOST_HOST = process.env.SPARKPOST_HOST;
 const { extname } = require("path");
 const converter = require("json-2-csv");
 
+//Environment varialbes pulled from the ../config/variables.env file
+dotenv.config({ path: path.resolve(__dirname, "../config/variables.env") });
+const SPARKPOST_API_KEY = process.env.SPARKPOST_API_KEY;
+const SPARKPOST_HOST = process.env.SPARKPOST_HOST;
+
 async function validateRecipients(email_count, myArgs) {
   if (
+    //If both the infile and outfile are in .csv format
     extname(myArgs[1]).toLowerCase() == ".csv" &&
     extname(myArgs[3]).toLowerCase() == ".csv"
   ) {
-    let completed = 0;
-    email_count++;
+    let completed = 0; //Counter for each API call
+    email_count++; //Line counter returns #lines - 1, this is done to correct the number of lines
+
+    //Start a timer
     const { window } = new JSDOM();
     const start = window.performance.now();
 
-    const output = fs.createWriteStream(myArgs[3]);
+    const output = fs.createWriteStream(myArgs[3]); //Outfile
     output.write(
       "Email,Delivery_Confidence,Is_Disposable,Is_Free,Is_Role,Result,Valid\n"
-    );
+    ); //Write the headers in the outfile
 
     fs.createReadStream(myArgs[1])
       .pipe(csv.parse({ headers: false }))
       .on("data", async (email) => {
         let url =
           SPARKPOST_HOST + "/api/v1/recipient-validation/single/" + email;
-        await require("axios")
+        await axios
           .get(url, {
             headers: {
               Authorization: SPARKPOST_API_KEY,
             },
-          })
+          }) //For each row read in from the infile, call the SparkPost Recipient Validation API
           .then(function (response) {
-            response.data.email = String(email);
+            response.data.email = String(email); //Adds the email as a value/key pair to the response JSON to be used for output
+
+            //Utilizes json-2-csv to convert the JSON to CSV format and output
             let options = {
-              prependHeader: false,
-              sortHeader: true,
+              prependHeader: false, //Disables JSON values from being added as header rows for every line
+              sortHeader: true, //Sorts the values
             };
             let json2csvCallback = function (err, csv) {
               if (err) throw err;
               output.write(`${csv}\n`);
             };
-
             converter.json2csv(response.data, json2csvCallback, options);
-            completed++;
-            process.stdout.write(`Done with ${completed} / ${email_count}\r`);
+
+            completed++; //Increase the API counter
+            process.stdout.write(`Done with ${completed} / ${email_count}\r`); //Output status of Completed / Total to the console without showing new lines
+
+            //If all emails have completed validation
             if (completed == email_count) {
-              const stop = window.performance.now();
+              const stop = window.performance.now(); //Stop the timer
               console.log(
                 `All emails successfully validated in ${
                   (stop - start) / 1000
